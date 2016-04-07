0467b3d356c92899f3a402c1545c5698a2bc80f7
sergey-koba-mobidev <s.koba@mobidev.biz>
2016-04-07 10:46:40 +0300

    temp refactoring

--- libs/tackle_environment.rb
+++ libs/tackle_environment.rb
@@ -1,3 +1,12 @@
-module TacleEnvironment
+class TackleEnvironment
 
+  def self.build(os = 'linux')
+
+  end
+
+  def initialize(docker_manager, config_manager, host_manager)
+    @docker_manager = docker_manager
+    @config_manager = config_manager
+    @host_manager = host_manager
+  end
 end

714182dd8491ab9b95a10c5a5bb9c7d7c03dde75
sergey-koba-mobidev <s.koba@mobidev.biz>
2016-04-13 18:07:20 +0300

    fix mzc dns resolver

--- libs/mac/host_manager.rb
+++ libs/mac/host_manager.rb
@@ -3,15 +3,16 @@ require './libs/base/host_manager_base'
 class HostManager < HostManagerBase
 
   def modify_dns(docker_ip)
-    #TODO file is overwritten by resolveconf
-    f = File.open('/etc/resolv.conf', 'r')
+    system("sudo sh -c 'mkdir /etc/resolver'")
+    system("sudo sh -c 'touch /etc/resolver/consul'")
+    f = File.open('/etc/resolver/consul', 'r')
     lines = f.readlines
     f.close
     if lines.grep(/#{docker_ip}/).size == 0
       # Prepend lines to a file
-      system("sudo sh -c 'echo \"nameserver #{docker_ip}\"|cat - /etc/resolv.conf > /tmp/out && mv /tmp/out /etc/resolv.conf'")
+      system("sudo sh -c 'echo \"nameserver #{docker_ip}\"|cat - /etc/resolver/consul > /tmp/out && mv /tmp/out /etc/resolver/consul'")
       #system("sudo sh -c 'echo \"search consul\"|cat - /etc/resolv.conf > /tmp/out && mv /tmp/out /etc/resolv.conf'")
-      puts "Host's DNS successfully modified. (/etc/resolv.conf)\n".green
+      puts "Host's DNS successfully modified. (/etc/resolver/consul)\n".green
     end
   end
 
